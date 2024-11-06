071bcfbd842e7f28b581419525e8017e3dbf536e
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-06 11:31:48 +0700

    Update Student Model and Migration

--- database/migrations/2024_11_06_042443_create_students_table.php
+++ database/migrations/2024_11_06_042443_create_students_table.php
@@ -15,7 +15,12 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('institution_id');
+            $table->string('name');
+            $table->boolean('is_deleted')->default(false);;
             $table->timestamps();
+
+            $table->foreign('institution_id')->references('id')->on('institutions')->onDelete('cascade');
         });
     }
 
