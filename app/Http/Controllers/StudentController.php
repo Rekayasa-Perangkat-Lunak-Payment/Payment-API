5cd9ef0a7b3c7c30ba527648899d7682f67a8f40
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-06 13:42:17 +0700

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,66 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json($students);
+    }
+
+    public function store(Request $request){
+        $student = new Student();
+        $student->name = $request->name;
+        $student->institution_id = $request->institution_id;
+        $student->save();
+        return response()->json([
+            'message' => 'Student created successfully',
+            'student' => $student
+        ], 201);
+    }
+
+    public function getStudent($id){
+        $student = Student::find($id);
+        if(!empty($student)){
+            return response()->json($student);
+        }else{
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id){
+        $student = Student::find($id);
+        if(!empty($student)){
+            $student->name = $request->name;
+            $student->institution_id = $request->institution_id;
+            $student->save();
+            return response()->json([
+                'message' => 'Student updated successfully',
+                'student' => $student
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
+
+    public function destroy($id){
+        $student = Student::find($id);
+        if(!empty($student)){
+            $student->delete();
+            return response()->json([
+                'message' => 'Student deleted successfully'
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
+    }
 }
