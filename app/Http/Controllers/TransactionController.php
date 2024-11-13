8332b2f5cd43bd77af31aee58fe18c54903f7824
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-13 18:59:34 +0700

    Modify route and create a transaction API

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TransactionController extends Controller
 {
-    //
+    /**
+     * Display a listing of transactions.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $transactions = Transaction::all();
+        return response()->json($transactions, Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created transaction in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'virtual_account_id' => 'required|exists:virtual_accounts,id',
+            'transaction_date' => 'required|date',
+            'total' => 'required|numeric|min:0',
+        ]);
+
+        $transaction = Transaction::create($request->all());
+
+        return response()->json($transaction, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified transaction.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $transaction = Transaction::find($id);
+
+        if (!$transaction) {
+            return response()->json(['message' => 'Transaction not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($transaction, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified transaction in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'virtual_account_id' => 'sometimes|exists:virtual_accounts,id',
+            'transaction_date' => 'sometimes|date',
+            'total' => 'sometimes|numeric|min:0',
+        ]);
+
+        $transaction = Transaction::find($id);
+
+        if (!$transaction) {
+            return response()->json(['message' => 'Transaction not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $transaction->update($request->all());
+
+        return response()->json($transaction, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified transaction from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $transaction = Transaction::find($id);
+
+        if (!$transaction) {
+            return response()->json(['message' => 'Transaction not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $transaction->delete();
+
+        return response()->json(['message' => 'Transaction deleted successfully'], Response::HTTP_NO_CONTENT);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\InstitutionController;
 use App\Http\Controllers\BankAdminController;
 use App\Http\Controllers\InstitutionAdminController;
-use App\Models\InstitutionAdmin;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,39 +23,8 @@ use App\Models\InstitutionAdmin;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// Student routes
-Route::prefix('students')->group(function () {
-    Route::get('/', [StudentController::class, 'index']);
-    Route::post('/', [StudentController::class, 'store']);
-    Route::get('/{id}', [StudentController::class, 'getStudent']);
-    Route::put('/{id}', [StudentController::class, 'update']);
-    Route::delete('/{id}', [StudentController::class, 'destroy']);
-});
-
-// Institution routes
-Route::prefix('institutions')->group(function () {
-    Route::get('/', [InstitutionController::class, 'index']);
-    Route::post('/', [InstitutionController::class, 'store']);
-    Route::get('/{id}', [InstitutionController::class, 'getInstitution']);
-    Route::put('/{id}', [InstitutionController::class, 'update']);
-    Route::delete('/{id}', [InstitutionController::class, 'destroy']);
-});
-
-// Institution admin routes
-Route::prefix('institution-admins')->group(function () {
-    Route::get('/', [InstitutionAdminController::class, 'index']);
-    Route::post('/', [InstitutionAdminController::class, 'store']);
-    Route::get('/{id}', [InstitutionAdminController::class, 'show']);
-    Route::put('/{id}', [InstitutionAdminController::class, 'update']);
-    Route::delete('/{id}', [InstitutionAdminController::class, 'destroy']);
-});
-
-// Bank admin routes
-Route::prefix('bank-admins')->group(function () {
-    Route::get('/', [BankAdminController::class, 'index']);
-    Route::post('/', [BankAdminController::class, 'store']);
-    Route::get('/{id}', [BankAdminController::class, 'show']);
-    Route::put('/{id}', [BankAdminController::class, 'update']);
-    Route::delete('/{id}', [BankAdminController::class, 'destroy']);
-});
+Route::apiResource('transactions', TransactionController::class);
+Route::apiResource('institutions', InstitutionController::class);
+Route::apiResource('students', StudentController::class);
+Route::apiResource('institution-admins', InstitutionAdminController::class);
+Route::apiResource('bank-admins', BankAdminController::class);
