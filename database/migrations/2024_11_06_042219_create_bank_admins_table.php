7fdb5c81230b9e82b891eae7e8c2e4f7aa73279f
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-19 17:17:39 +0700

    Update 2024_11_06_042219_create_bank_admins_table.php

--- database/migrations/2024_11_06_042219_create_bank_admins_table.php
+++ database/migrations/2024_11_06_042219_create_bank_admins_table.php
@@ -17,6 +17,7 @@ class CreateBankAdminsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('name');
+            $table->string('nik');
             $table->timestamps();
         });
     }
