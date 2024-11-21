eb1362271396c54db2cc6e4f474f3baa0a6fb767
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-21 14:06:54 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,14 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // Create adminbank user
         User::create([
             'username' => 'adminbank',
             'email' => 'admin@example.com',
             'password' => bcrypt('123123'),
         ]);
 
-        // Create adminsekolah user
         User::create([
             'username' => 'adminsekolah',
             'email' => 'admin@example.com',
