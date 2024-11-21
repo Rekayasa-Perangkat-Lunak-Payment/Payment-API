500f3f65c681cf07d74ca9cb3b1108618d6ba523
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-21 09:04:28 +0700

    Create 2024_11_21_020158_create_item_types_table.php

---
+++ database/migrations/2024_11_21_020158_create_item_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('institution_id');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_types');
+    }
+}
