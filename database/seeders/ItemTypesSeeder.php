3f0b6ab5ca2489c3beb1ef1389d06a01dcee2bfe
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-21 13:17:12 +0700

    Create ItemTypesSeeder.php

---
+++ database/seeders/ItemTypesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ItemTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
